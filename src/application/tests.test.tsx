af8657c87d72161f080e9862257f59aa3b3ab773
Isabelly <isabelly.marques@wicomm.com.br>
2026-04-23 09:21:13 -0300

    feat: adição de testes

--- src/application/tests.test.tsx
+++ src/application/tests.test.tsx
@@ -1,6 +1,8 @@
-import locations from '../application/Locations'
+import locations from './Locations'
+import Pagination from './Pagination'
 
 import '@testing-library/jest-dom'
+import {render, screen} from '@testing-library/react';
 
 describe('tests tourism application', () => {
     test('locations is not an empty array', () => {
@@ -39,7 +41,7 @@ describe('tests tourism application', () => {
         expect(arr.length).toBeGreaterThan(0)
     })
 
-     test('if state accepts parcial search', () => {
+    test('if state accepts parcial search', () => {
         const arr = locations.filter((location) => location.state.includes('Rio'))
         
         expect(arr.length).toBeGreaterThan(0)
@@ -51,6 +53,30 @@ describe('tests tourism application', () => {
         expect(arr.length).toBeGreaterThan(0)
     })
 
+    test('if returns all when inputs are empty', () => {
+        const stateInput = ''
+        const cityInput = ''
+
+        const result = locations.filter((location) =>
+            location.state.includes(stateInput) &&
+            location.city.includes(cityInput)
+        )
+
+        expect(result.length).toBeGreaterThan(0)
+    })
+
+    test('if returns nothing when one value exists in array but the other doesnt', () => {
+        const stateInput = 'Rio'
+        const cityInput = 'Oi'
+
+        const result = locations.filter((location) =>
+            location.state.includes(stateInput) &&
+            location.city.includes(cityInput)
+        )
+
+        expect(result).toHaveLength(0)
+    })
+
     test('if combined search works', () => {
         const result = locations.filter((location) =>
             location.state.includes('Rio') &&
@@ -58,5 +84,39 @@ describe('tests tourism application', () => {
         )
 
         expect(result.length).toBeGreaterThan(0)
+    })
+
+    test('if pagination is rendering correct number of buttons', () => {
+        const mockCallback = jest.fn
+        
+        render(
+            <Pagination 
+                totalLocations={5}
+                perPage={2}
+                setCurrentPage={mockCallback}
+                currentPage={1}
+            />
+        )
+
+        const buttons = screen.queryAllByRole('button')
+        expect(buttons).toHaveLength(3)
+    })
+
+    test('if there are no pages if locations is an empty array', () => {
+        const mockCallback = jest.fn
+
+        const locations = []
+        
+        render(
+            <Pagination 
+                totalLocations={locations.length}
+                perPage={2}
+                setCurrentPage={mockCallback}
+                currentPage={1}
+            />
+        )
+
+        const buttons = screen.queryAllByRole('button', { hidden: true })
+        expect(buttons).toHaveLength(0)
     })
 })
